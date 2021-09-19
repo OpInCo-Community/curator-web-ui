6e8223ae0cd80182fcc3c77365aa843fa854d068
fazer1929 <abhishekagrawal8888@gmail.com>
2021-09-19 10:22:38 +0530

    React Redux Setup

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+// import sampleReducer from "./sample";
+
+const rootReducer = combineReducers({
+  //   sample: sampleReducer,
+});
+
+export default rootReducer;
