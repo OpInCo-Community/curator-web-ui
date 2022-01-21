c45c8a2fc54aa27181ced3e8a9b26489bcda49bc
fazer1929 <abhishekagrawal8888@gmail.com>
2022-01-21 14:09:45 +0530

    created curation card

--- src/Components/CurationCard/CurationCard.jsx
+++ src/Components/CurationCard/CurationCard.jsx
@@ -5,7 +5,34 @@ import styles from './CurationCard.module.css';
 export default function CurationCard({upvotes,title,description,subject,author}) {
     return (
         <Card>
-        
+          <Container>
+          <Row>
+            <Col xs={3}>
+            RWPPP
+            </Col>
+            <Col xs={9}>
+              <Card.Header as="h2">
+    <Card.Title>Top 450 DSA</Card.Title>
+  </Card.Header>
+  <Card.Body>
+    <Card.Text>
+      With supporting text below as a natural lead-in to additional content.
+    </Card.Text>
+    
+    <Card.Text>
+        <Link to='/'>
+            Computer Science
+        </Link>
+        <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+        <Link to='/'>
+            Author
+        </Link>
+    </Card.Text>
+    
+  </Card.Body>
+            </Col>
+            </Row>
+</Container>  
 </Card>
     )
 }
